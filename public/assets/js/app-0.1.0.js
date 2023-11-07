7bf68f9ec11fc8442116109a42ddee6d93a8c055
Ryan Dawes <dev@dawes.cc>
2023-11-07 11:32:08 -0500

    formatted date with momement

--- public/assets/js/app-0.1.0.js
+++ public/assets/js/app-0.1.0.js
@@ -1,1 +1,9 @@
 console.log('hi!');
+
+function formatDateWithMoment(isoString) {
+    const momentDate = moment(isoString);
+    const formattedDate = momentDate.tz('America/New_York').format('YYYY-MM-DD HH:mm:ss z');
+    const relativeTime = momentDate.fromNow();
+  
+    return `${formattedDate} (${relativeTime})`;
+};
