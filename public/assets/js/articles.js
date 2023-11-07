ee13101294c811e20621854901be3e7b514ba952
Ryan Dawes <dev@dawes.cc>
2023-11-07 11:36:13 -0500

    fixed time formatting

--- public/assets/js/articles.js
+++ public/assets/js/articles.js
@@ -1,6 +1,6 @@
 function formatDateWithMoment(isoString) {
     const momentDate = moment(isoString);
-    const formattedDate = momentDate.tz('America/New_York').format('YYYY-MM-DD HH:mm:ss z');
+    const formattedDate = momentDate.locale('America/New_York').format('YYYY-MM-DD HH:mm:ss z');
     const relativeTime = momentDate.fromNow();
   
     return `${formattedDate} (${relativeTime})`;
