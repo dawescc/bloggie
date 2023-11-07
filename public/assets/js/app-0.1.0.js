5d1a6c84aac8b9f6a6f2488ea27416a3dd369583
Ryan Dawes <dev@dawes.cc>
2023-11-07 10:58:35 -0500

    toying

--- public/assets/js/app-0.1.0.js
+++ public/assets/js/app-0.1.0.js
@@ -1,41 +1,41 @@
-function dataFetcher() {
-    return {
-      articles: [],
-      topics: [],
-      selectedTag: null,
-      async init() {
-        await this.fetchData();
-      },
-      async fetchData() {
-        const url = 'https://neutgwolrftsfsvfhutp.supabase.co/rest/v1/articles';
-        const headers = {
-          'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldXRnd29scmZ0c2ZzdmZodXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzNjY4ODYsImV4cCI6MjAxNDk0Mjg4Nn0.jeV6s1rVlm-l-Tgny8lPq0CQheCq10ch7xyjQ0P2G84',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldXRnd29scmZ0c2ZzdmZodXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzNjY4ODYsImV4cCI6MjAxNDk0Mjg4Nn0.jeV6s1rVlm-l-Tgny8lPq0CQheCq10ch7xyjQ0P2G84',
-          'Content-Type': 'application/json'
-        };
-  
-        try {
-          const response = await fetch(url, { headers });
-          if (!response.ok) throw new Error('Network response was not ok');
-          this.articles = await response.json();
-          this.extractTopics();
-        } catch (error) {
-          console.error('Error fetching data from Supabase:', error);
-        }
-      },
-      extractTopics() {
-        this.topics = Array.from(new Set(this.articles.flatMap(article => article.tags)));
-      },
-      get filteredArticles() {
-        return this.selectedTag 
-          ? this.articles.filter(article => article.tags.includes(this.selectedTag))
-          : this.articles;
+// Initialize Supabase client
+const supabaseUrl = 'https://neutgwolrftsfsvfhutp.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldXRnd29scmZ0c2ZzdmZodXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzNjY4ODYsImV4cCI6MjAxNDk0Mjg4Nn0.jeV6s1rVlm-l-Tgny8lPq0CQheCq10ch7xyjQ0P2G84';
+const supabase = Supabase.createClient(supabaseUrl, supabaseAnonKey);
+
+document.addEventListener('alpine:init', () => {
+  Alpine.data('dataFetcher', () => ({
+    articles: [],
+    topics: [],
+    selectedTag: null,
+    async fetchData() {
+      let { data: articles, error } = await supabase
+        .from('articles')
+        .select('*');
+
+      if (error) {
+        console.error('Error fetching articles:', error);
+        return;
       }
-    };
-  }
-  
-  // In your HTML, after Alpine.js is loaded:
-  document.addEventListener('alpine:init', () => {
-    Alpine.data('dataFetcher', dataFetcher);
-  });
-  
+
+      this.articles = articles;
+      this.topics = this.extractTopics(articles);
+    },
+    extractTopics(articles) {
+      const topics = new Set();
+      articles.forEach(article => {
+        article.tags.forEach(tag => topics.add(tag));
+      });
+      return Array.from(topics);
+    },
+    get filteredArticles() {
+      if (!this.selectedTag) {
+        return this.articles;
+      }
+      return this.articles.filter(article => article.tags.includes(this.selectedTag));
+    },
+    init() {
+      this.fetchData();
+    },
+  }));
+});
