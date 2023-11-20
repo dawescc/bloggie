293a73081f014b5e0174e11dfd34797f7ab833c1
Ryan Dawes <dev@dawes.cc>
2023-11-19 20:33:04 -0500

    removed misc, updated profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,32 +1,45 @@
 const Profile = ({ props }) => {
 	return (
-		<div className='w-full flex flex-col items-center p-4 pt-8 pb-6 border-b border-zinc-200 dark:border-zinc-700 '>
-			<div className='flex flex-col w-full rounded-lg shadow-sm border border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-800 overflow-hidden'>
-				<span className='h-3/4 flex gap-1 dark:bg-zinc-900 bg-zinc-300 bg-opacity-50 dark:bg-opacity-50 border-b border-zinc-200 dark:border-zinc-700 px-3 pt-3 pb-2 overflow-auto'>
-					<div className='flex flex-col gap-2'>
-						<div className='h-24 w-20 rounded-lg overflow-hidden border-zinc-200 dark:border-zinc-700 border'>
-							<img className='object-cover w-full h-full' src='https://neutgwolrftsfsvfhutp.supabase.co/storage/v1/object/public/blog/16bmtkif9g3i.png'/>
-						</div>
-						<span className='text-[1.5rem]/[1.5rem] font-semibold'>ryan</span>
-						<span className='text-[0.75rem]/[1.5rem] font-light'>football fan, developer</span>
+		<div
+			className='w-full flex flex-col gap-4 px-8 pt-20 pb-8 border-b border-zinc-200 dark:border-zinc-700
+			bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100
+			dark:bg-gradient-to-r dark:from-zinc-700 dark:via-zinc-900 dark:to-black
+			'>
+			{/* Card Container */}
 
-					</div>
-				</span>
-
-				<span className='h-1/4 px-3 py-2 flex gap-2 flex-wrap'>
-					<a href='https://dawes.cc' className='hover:underline text-[0.65rem]/[0.65rem] flex
-						 dark:bg-zinc-900 bg-zinc-300 bg-opacity-50 dark:bg-opacity-50 items-center justify-center rounded-md px-2 py-1
-						 border-zinc-300 dark:border-zinc-700 border hover:bg-opacity-10'>
-							<i className="fa-solid fa-globe"></i>&nbsp;&nbsp;dawes.cc
-						 </a>
-
-					<a href='https://twitter.com/dawescc' className='hover:underline text-[0.65rem]/[0.65rem] flex
-						 dark:bg-zinc-900 bg-zinc-300 bg-opacity-50 dark:bg-opacity-50 items-center justify-center rounded-md px-2 py-1
-						 border-zinc-300 dark:border-zinc-700 border hover:bg-opacity-10'>
-							<i className="fa-brands fa-twitter"></i>&nbsp;&nbsp;@dawescc
-						 </a>
-				</span>
+			{/* Image */}
+			<div
+				className='h-24 w-24 aspect-square overflow-hidden
+					 border-zinc-100 dark:border-zinc-800 border-4
+					 shadow-md rounded-xl'>
+				<img
+					className='object-cover w-full h-full'
+					src='https://neutgwolrftsfsvfhutp.supabase.co/storage/v1/object/public/blog/16bmtkif9g3i.png'
+				/>
 			</div>
+
+			<div className='flex flex-col gap-4 text-zinc-950 dark:text-zinc-100 mr-auto'>
+				<span className='font-bold text-2xl'>ryan</span>
+				<span className='text-lg'>football fan + developer</span>
+			</div>
+
+			<span className='w-full px-3 py-2 flex gap-2 flex-wrap bg-zinc-100 dark:bg-zinc-800 rounded-lg shadow-md border border-zinc-200 dark:border-zinc-700'>
+					<a
+						href='https://dawes.cc'
+						className='hover:underline text-sm flex flex-grow
+						 dark:bg-zinc-900 bg-zinc-300 bg-opacity-50 dark:bg-opacity-50 items-center justify-center rounded-md px-2 py-1
+						 border-zinc-300 dark:border-zinc-700 border hover:bg-opacity-10'>
+						<i className='fa-solid fa-globe'></i>
+					</a>
+
+					<a
+						href='https://twitter.com/dawescc'
+						className='hover:underline text-sm flex flex-grow
+						 dark:bg-zinc-900 bg-zinc-300 bg-opacity-50 dark:bg-opacity-50 items-center justify-center rounded-md px-2 py-1
+						 border-zinc-300 dark:border-zinc-700 border hover:bg-opacity-10'>
+						<i className='fa-brands fa-twitter'></i>
+					</a>
+			</span>
 		</div>
 	);
 };
