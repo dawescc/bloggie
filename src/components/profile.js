1540956ac767871691588016c1d4e83d588140fd
Ryan Dawes <dev@dawes.cc>
2023-11-20 17:19:06 -0500

    adde pfp, updated style

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,45 +1,32 @@
 const Profile = ({ props }) => {
 	return (
-		<div
-			className='w-full flex flex-col gap-4 px-8 pt-16 pb-8 border-b border-zinc-200 dark:border-zinc-700
-			bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100
-			dark:bg-gradient-to-r dark:from-zinc-700 dark:via-zinc-900 dark:to-black
-			'>
-			{/* Card Container */}
+		<div className='w-full h-52 flex overflow-hidden
+						border-b border-zinc-200 dark:border-zinc-700
+						bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100'>
 
-			{/* Image */}
-			<div
-				className='h-24 w-24 aspect-square overflow-hidden
-					 border-zinc-100 dark:border-zinc-800 border-4
-					 shadow-md rounded-xl'>
-				<img
-					className='object-cover w-full h-full'
-					src='https://neutgwolrftsfsvfhutp.supabase.co/storage/v1/object/public/blog/16bmtkif9g3i.png'
-				/>
+			<div className="w-full h-full flex pt-10 px-6 gap-2">
+				<div className="flex-grow"></div>
+				<div className='h-full flex flex-col gap-4 text-zinc-950'>
+					<span className='font-bold text-2xl'>ryan</span>
+					<span className='text-lg'>football fan + developer</span>
+					<span className='w-full flex gap-4 items-center content-center flex-wrap text-xs'>
+						<a
+							href='https://dawes.cc'
+							className='underline'>
+							website
+						</a>
+
+						<a
+							href='https://github.com/dawescc'
+							className='underline'>
+							github
+						</a>
+					</span>
+				</div>
+				<img src="images/ryan.svg" className="w-1/3 h-full translate-y-2"/>
+				<div className="flex-grow"></div>
 			</div>
 
-			<div className='flex flex-col gap-4 text-zinc-950 dark:text-zinc-100 mr-auto'>
-				<span className='font-bold text-2xl'>ryan</span>
-				<span className='text-lg'>football fan + developer</span>
-			</div>
-
-			<span className='w-full px-3 py-2 flex gap-2 flex-wrap bg-zinc-100 dark:bg-zinc-800 rounded-lg shadow-md border border-zinc-200 dark:border-zinc-700'>
-					<a
-						href='https://dawes.cc'
-						className='hover:underline text-sm flex flex-grow
-						 dark:bg-zinc-900 bg-zinc-300 bg-opacity-50 dark:bg-opacity-50 items-center justify-center rounded-md px-2 py-1
-						 border-zinc-300 dark:border-zinc-700 border hover:bg-opacity-10'>
-						<i className='fa-solid fa-globe'></i>
-					</a>
-
-					<a
-						href='https://twitter.com/dawescc'
-						className='hover:underline text-sm flex flex-grow
-						 dark:bg-zinc-900 bg-zinc-300 bg-opacity-50 dark:bg-opacity-50 items-center justify-center rounded-md px-2 py-1
-						 border-zinc-300 dark:border-zinc-700 border hover:bg-opacity-10'>
-						<i className='fa-brands fa-twitter'></i>
-					</a>
-			</span>
 		</div>
 	);
 };
