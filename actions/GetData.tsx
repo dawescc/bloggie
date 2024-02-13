9f6cf4fa63843d411086b2eaebc8d3a03fa34b9d
Ryan Dawes <dev@dawes.cc>
2024-02-13 10:59:17 -0500

    swapped data to server action

---
+++ actions/GetData.tsx
@@ -0,0 +1,14 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function getData() {
+	const cookieStore = cookies();
+	const supabase = createClient(cookieStore);
+	try {
+		let { data: articles, error } = await supabase.from("articles_dev_replies").select("*").order("id", { ascending: false });
+		if (error) throw error;
+		return articles;
+	} catch (error) {
+		console.error("Unable to load articles from database", error);
+	}
+}
--- components/Content.tsx
+++ components/Content.tsx
@@ -1,18 +1,5 @@
 import Feed from "@/components/Feed";
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
-
-async function getData() {
-	const cookieStore = cookies();
-	const supabase = createClient(cookieStore);
-	try {
-		let { data: articles, error } = await supabase.from("articles_dev_replies").select("*").order("id", { ascending: false });
-		if (error) throw error;
-		return articles;
-	} catch (error) {
-		console.error("Unable to load articles from database", error);
-	}
-}
+import getData from "@/actions/GetData";
 
 export default async function Content() {
 	const articles = await getData();
